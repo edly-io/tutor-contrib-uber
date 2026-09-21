f7a5619f35a35febcedece5dd9a875209c63158c
Ali Tariq <raja.ali945@gmail.com>
2026-09-21 16:34:54 +0500

    chore: formatting fix and type annotation

--- tutoruber/plugin.py
+++ tutoruber/plugin.py
@@ -287,6 +287,7 @@ for path in glob(str(importlib_resources.files("tutoruber") / "patches" / "*")):
 # This would allow you to run:
 #   $ tutor uber example-command
 
+
 @hooks.Filters.DOCKER_BUILD_COMMAND.add()
-def enable_ssh_forwarding(cmd):
+def enable_ssh_forwarding(cmd: list[str]) -> list[str]:
     return cmd + ["--ssh", "default"]
